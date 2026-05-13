80891c90780d01c2d977e2116b6faf0f1f71a882
Pas Anna <anya.pas05@gmail.com>
2026-05-13 11:55:56 +0300

    Лаба 3

--- nails/views.py
+++ nails/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    # Збираємо головну сторінку
+    return render(request, 'index.html', {'title': 'Манікюрний салон - Головна'})
+
+
+def other(request):
+    # Збираємо іншу сторінку
+    return render(request, 'other.html', {'title': 'Наші послуги'})
+
