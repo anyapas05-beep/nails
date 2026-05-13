3a94e6db71be3dfad07b64ef69ee1e9cb4d768f7
Pas Anna <anya.pas05@gmail.com>
2026-05-14 01:03:45 +0300

    Лаба 4

--- nails/admin.py
+++ nails/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from .models import Category, Brand, Product
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    # Додаємо 'price' назад у list_display, щоб працював list_editable
+    list_display = ('get_html_photo', 'name', 'brand', 'category', 'price', 'formatted_price', 'created_at')
+
+    list_display_links = ('get_html_photo', 'name')
+    search_fields = ('name', 'description')
+    list_filter = ('category', 'brand', 'created_at')
+
+    # Тепер 'price' є у list_display, і помилка зникне
+    list_editable = ('price',)
+
+    def get_html_photo(self, object):
+        if object.image:
+            return mark_safe(f"<img src='{object.image.url}' width=50 style='border-radius: 5px;'>")
+        return "Немає фото"
+
+    get_html_photo.short_description = "Мініатюра"
+
+    def formatted_price(self, obj):
+        return f"{obj.price} грн"
+
+    formatted_price.short_description = "Ціна (гарна)"
+    formatted_price.admin_order_field = 'price'
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
--- nails/models.py
+++ nails/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+# 1. Категорії товарів (напр. Лаки, Лампи, Інструменти)
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Категорія")
+
+    def __str__(self):
+        return self.name
+
+
+# 2. Бренди (напр. Kodi, Staleks, Komilfo)
+class Brand(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Бренд")
+
+    def __str__(self):
+        return self.name
+
+
+# 3. Товари для манікюру
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категорія")
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, verbose_name="Бренд")
+
+    name = models.CharField(max_length=150, verbose_name="Назва товару")
+    description = models.TextField(verbose_name="Опис товару", blank=True, null=True)
+    image = models.ImageField(upload_to='products/', verbose_name="Фото товару", blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Ціна")
+
+    # Автоматичні дати (вимога лаби)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Додано")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Оновлено")
+
+    def __str__(self):
+        return self.name
